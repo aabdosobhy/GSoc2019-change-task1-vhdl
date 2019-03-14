3b2564d3cc0f2275acd4e3b43a69d2e6e7cf799a
aabdosobhy <aabdosobhy@gmail.com>
2019-03-14 19:33:50 +0200

    latest version before code formating

--- ser2par.vhd
+++ ser2par.vhd
@@ -9,7 +9,6 @@ entity ser2par is
 		start : in std_logic;					     -- start signal 
 		serialBit :in std_logic ;				     -- serial bit from the serial line 
 		bitslip: in std_logic_vector( 1 downto 0);		     -- adjusted output word size 
-		enable_read_dataReady : out std_logic;			     -- the data is ready to read for the parallel 
 		parallelOut : out std_logic_vector (parallelSize-1 downto 0) -- output parallel word
 	);    
 
@@ -18,17 +17,6 @@ end entity ser2par;
 
 architecture ser2par_A of ser2par is
 	
---component mux4_1 is 
---	generic ( 
---		data_size : integer := 12   						  -- input and output size 
---	); 
---	port (
---		input1,input2,input3,input4 : in std_logic_vector(data_size-1 downto 0);  -- four inputs to the mux 
---		selection:  in std_logic_vector (1 downto 0);				  -- selection lines 
---		output : out std_logic_vector(data_size-1 downto 0)    			  -- output of the mux end component;
---	);
---end component;
-
 
 component nRegister_F is
 	generic (
@@ -68,18 +56,6 @@ component counter_f is
 end component;
 
 
---component counter_R is
---  	generic (
---		n:integer :=4
---	); 
---  	Port ( 
---		clk: in std_logic;			   	-- input clock
---		start: in std_logic;				-- input start system 
---		rst : in std_logic;			   	-- counter Reset  
---   		count : out std_logic_vector( n-1 downto 0 )	-- counter output 
---	);
-
---end component;
 
 
 
@@ -134,16 +110,6 @@ Reg_shift : nRegister
 
 
 
---Reg_out	: nRegister_F
---	generic map (
---		size => parallelSize)
---	port map(
---		clk => clk,
---		enb => en_write_OutReg,
---		rst => start,
---		d => out_Reg_shift,
---		q => parallelOut );
-
 
 
 counter_4b_f : counter_f 
@@ -155,31 +121,23 @@ counter_4b_f : counter_f
 		rst =>rst_counter,
    		count =>count );
 
---counter_4b_R : counter_R 
---  	generic map (
---		n=>4)
---  	port map ( 
---		clk => clk,
---		start => start,
---		rst => rst_counter_rising,
---   		count => count_rising );
-
 	
 
 	
-	process (select_out,serialBit,data_8b,data_10b,data_12b,start)
+	process (select_out,serialBit,out_Reg_shift)
 	begin 
+			shift_word_in<= (others=>'0');
 		
 			if select_out = "00"  then 
-				shift_word_in<= serialBit & data_8b &"0000";
+				shift_word_in<= serialBit & out_Reg_shift(11 downto 5) &"0000";
 
 			elsif select_out = "01"  then 
-				shift_word_in<= serialBit & data_10b &"00";
+				shift_word_in<= serialBit & out_Reg_shift(11 downto 3) &"00";
 
-			elsif select_out(1) = '1'  then 
-				shift_word_in<= serialBit & data_12b ;
-			elsif start ='1' then 
-				shift_word_in <= (others=>'0');
+			elsif select_out = "10" or select_out = "11"   then 
+				shift_word_in<= serialBit & out_Reg_shift(11 downto 1) ;
+			--elsif start ='1' then 
+			--	shift_word_in <= (others=>'0');
 
 			end if ;
 
@@ -194,7 +152,7 @@ count_10 <= count(3) and (not count(2)) and  count(1) and (not count(0)) and (no
 count_12 <= count(3) and count(2) and not (count(1)) and (not count(0)) and select_out(1);
 count_0  <=  (not count(3)) and (not count(2)) and (not count(1)) and (not count(0));
 count_1 <= (not count(3)) and (not count(2)) and (not count(1)) and  count(0);
---countR_0 <=(not count_rising(3)) and (not count_rising(2)) and (not count_rising(1)) and (not count_rising(0));
+
 
 data_ready_out	 <= count_8 or count_10 or count_12 ;
 
@@ -204,15 +162,11 @@ rst_counter <=data_ready_out ;
 
 rst_counter_rising <= count_8 or count_10 or count_12 ;
    		
-data_8b  <= out_Reg_shift(11 downto 5);
-data_10b <= out_Reg_shift(11 downto 3);
-data_12b <= out_Reg_shift(11 downto 1);
 
 parallelOut<=out_Reg_shift;
-enable_read_dataReady<=data_ready_out;
---en_write_OutReg <= countR_0;
 
 
 end ser2par_A;
+
 
 
